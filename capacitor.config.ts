ef384d455ef3035c6547232803364db7a2ce20f8
imripon <98225619+imripon@users.noreply.github.com>
2023-03-19 22:38:26 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shortsn.test',
+  appName: 'status bar',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
